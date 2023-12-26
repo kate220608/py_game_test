48a93d93d39fa137f396e3cb62c6bac8f9b13f90
kate220608 <katyfar2008@mail.ru>
2023-12-26 12:23:03 +0300

    FIRST COMMIT

--- game.py
+++ game.py
@@ -1,15 +1,21 @@
 import pygame
 import os
 import sys
-import random
-
 
 pygame.init()
-size = width, height = 500, 500
+size = WIDTH, HEIGHT = 500, 500
 screen = pygame.display.set_mode(size)
-v = 1
-fps = 30
 clock = pygame.time.Clock()
+
+
+def load_level(filename):
+    filename = "data/" + filename
+    with open(filename, 'r') as mapFile:
+        level_map = [line.strip() for line in mapFile]
+
+    max_width = max(map(len, level_map))
+
+    return list(map(lambda x: list(x.ljust(max_width, '.')), level_map))
 
 
 def load_image(name, colorkey=None):
@@ -29,37 +35,124 @@ def load_image(name, colorkey=None):
     return image
 
 
-class Bomb(pygame.sprite.Sprite):
-    image = load_image("bomb.png")
-    image_boom = load_image("boom.png")
+tile_images = {
+    'wall': load_image('box.png'),
+    'empty': load_image('grass.png')
+}
+player_image = load_image('mario.png')
 
-    def __init__(self, group):
-        # НЕОБХОДИМО вызвать конструктор родительского класса Sprite.
-        # Это очень важно !!!
-        super().__init__(group)
-        self.image = Bomb.image
-        self.rect = self.image.get_rect()
-        self.rect.x = random.randrange(width - 50)
-        self.rect.y = random.randrange(height - 51)
-
-    def update(self, *args):
-        if args and args[0].type == pygame.MOUSEBUTTONDOWN and \
-                self.rect.collidepoint(args[0].pos):
-            self.image = self.image_boom
+tile_width = tile_height = 50
 
 
-all_sprites = pygame.sprite.Group()
-for _ in range(20):
-    Bomb(all_sprites)
+class Tile(pygame.sprite.Sprite):
+    def __init__(self, tile_type, pos_x, pos_y):
+        super().__init__(tiles_group)
+        self.image = tile_images[tile_type]
+        self.tile_type = tile_type
+        self.rect = self.image.get_rect().move(
+            tile_width * pos_x, tile_height * pos_y)
 
-if __name__ == '__main__':
-    running = True
-    while running:
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, pos_x, pos_y):
+        super().__init__(player_group)
+        self.image = player_image
+        self.rect = self.image.get_rect().move(tile_width * pos_x + 15, tile_height * pos_y + 5)
+        self.pos = (pos_x, pos_y)
+
+    def move(self, key):
+        pos = self.pos
+        if key == pygame.K_UP:
+            self.pos = self.pos[0], self.pos[1] - 1
+            self.rect = self.image.get_rect().move(tile_width * self.pos[0] + 15,
+                                                   tile_height * self.pos[1] + 5)
+        elif key == pygame.K_DOWN:
+            self.pos = self.pos[0], self.pos[1] + 1
+            self.rect = self.image.get_rect().move(tile_width * self.pos[0] + 15,
+                                                   tile_height * self.pos[1] + 5)
+        elif key == pygame.K_RIGHT:
+            self.pos = self.pos[0] + 1, self.pos[1]
+            self.rect = self.image.get_rect().move(tile_width * self.pos[0] + 15,
+                                                   tile_height * self.pos[1] + 5)
+        elif key == pygame.K_LEFT:
+            self.pos = self.pos[0] - 1, self.pos[1]
+            self.rect = self.image.get_rect().move(tile_width * self.pos[0] + 15,
+                                                   tile_height * self.pos[1] + 5)
+        if pygame.sprite.spritecollideany(self, tiles_group).tile_type == 'wall':
+            self.pos = pos
+            self.rect = self.image.get_rect().move(tile_width * self.pos[0] + 15,
+                                                   tile_height * self.pos[1] + 5)
+
+
+tiles_group = pygame.sprite.Group()
+player_group = pygame.sprite.Group()
+
+
+def generate_level(level):
+    new_player, x, y = None, None, None
+    for y in range(len(level)):
+        for x in range(len(level[y])):
+            if level[y][x] == '.':
+                Tile('empty', x, y)
+            elif level[y][x] == '#':
+                Tile('wall', x, y)
+            elif level[y][x] == '@':
+                Tile('empty', x, y)
+                level[y][x] = '.'
+                new_player = Player(x, y)
+    return new_player, x, y
+
+
+FPS = 50
+
+
+def terminate():
+    pygame.quit()
+    sys.exit()
+
+
+def start_screen():
+    intro_text = ["Перемещение героя", "",
+                  "Правила игры",
+                  "Игрок двигается",
+                  "при нажатии кнопок"]
+
+    fon = pygame.transform.scale(load_image('fon.jpg'), (WIDTH, HEIGHT))
+    screen.blit(fon, (0, 0))
+    font = pygame.font.Font(None, 30)
+    text_coord = 50
+    for line in intro_text:
+        string_rendered = font.render(line, 1, pygame.Color('black'))
+        intro_rect = string_rendered.get_rect()
+        text_coord += 10
+        intro_rect.top = text_coord
+        intro_rect.x = 10
+        text_coord += intro_rect.height
+        screen.blit(string_rendered, intro_rect)
+
+    while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                running = False
-            all_sprites.update(event)
-        screen.fill((0, 0, 0))
-        all_sprites.draw(screen)
+                terminate()
+            elif event.type == pygame.KEYDOWN or \
+                    event.type == pygame.MOUSEBUTTONDOWN:
+                return  # начинаем игру
         pygame.display.flip()
-    pygame.quit()
+        clock.tick(FPS)
+
+
+if __name__ == '__main__':
+    pygame.display.set_caption('Марио')
+    start_screen()
+    level_map = load_level(input())
+    player, level_x, level_y = generate_level(level_map)
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                terminate()
+            elif event.type == pygame.KEYDOWN:
+                player.move(event.key)
+        screen.fill(pygame.Color('black'))
+        tiles_group.draw(screen)
+        player_group.draw(screen)
+        pygame.display.flip()
